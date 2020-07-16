422342c954797b86533c2a53d4979d6196229a54
mit <mitbpatel0128@gmail.com>
2020-07-16 17:16:58 +0530

    Create MainActivity.java

---
+++ app/src/main/java/com/example/activities/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+public class MainActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Name = (EditText)findViewById(R.id.etName);
+        Password = (EditText)findViewById(R.id.etPassword);
+        Login = (Button)findViewById(R.id.btnLogin);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+    }
+
+    private void validate(String userName, String userPassword){
+        if((userName.equals("MIT")) && (userPassword.equals("01234"))){
+            Intent intent = new Intent(MainActivity.this, FirstActivity.class);
+            startActivity(intent);
+        }else{
+            Toast.makeText(getApplicationContext(),
+                    "Please enter correct credential ",
+                    Toast.LENGTH_SHORT).show();
+
+        }
+    }
+}
